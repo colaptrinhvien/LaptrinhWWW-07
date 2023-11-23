a3805a2f4889ade3ea622787509c8a05db0e9e04
colaptrinhvien <duyenphamphuoc.dev@gmail.com>
2023-11-23 19:59:17 +0700

    BTTHTuan07

---
+++ src/main/java/com/example/SpringBoot_QLBH/SpringBootQlbhApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_QLBH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQlbhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQlbhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_QLBH/SpringBootQlbhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_QLBH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQlbhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
